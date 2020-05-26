7374df23d2a96f90b8af50281aa5011f13aba5fe
Mihir Salunke <salunke.mi@husky.neu.edu>
2020-05-25 17:46:44 -0700

    First commit

---
+++ ChessApp/ChessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChessApp
+//
+//  Created by Mihir Salunke on 5/25/20.
+//  Copyright © 2020 Mihir Salunke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
